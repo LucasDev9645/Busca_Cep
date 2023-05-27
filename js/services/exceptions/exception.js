437b2a2f355a9635c2edb36da4807887ae574474
LucasDev <LucasDev9645@gmail.com>
2023-05-27 16:08:13 -0300

    correction

--- js/services/exceptions/exception.js
+++ js/services/exceptions/exception.js
@@ -1,5 +1,3 @@
-// https://rollbar.com/guides/javascript/how-to-throw-exceptions-in-javascript/
-
 export default function RequestException(message) {
   const error = new Error(message);
   return error;
